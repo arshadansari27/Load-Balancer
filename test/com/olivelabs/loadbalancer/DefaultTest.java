9d6f623ff7b584e20c66c157639b549091d314b0
arshadansari27 <arshadansari27@ba3f5f63-9442-bb69-0387-a3d508458a7b>
2010-09-24 21:35:23 +0000

    Initial Ant Build File, Default Test Case and Lib Folders with their JUnit jar

---
+++ test/com/olivelabs/loadbalancer/DefaultTest.java
@@ -0,0 +1,12 @@
+package com.olivelabs.loadbalancer;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DefaultTest {
+
+	@Test
+    public void testJUnit() {
+        System.out.println("This is default JUnit Test case.");
+    } 
+}
