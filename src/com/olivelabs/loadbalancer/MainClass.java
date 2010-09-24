a84388cc362e84dc5a17f6cdd450ed13e6168bca
arshadansari27 <arshadansari27@ba3f5f63-9442-bb69-0387-a3d508458a7b>
2010-09-24 14:56:35 +0000

    First Commit for this project. I am ashamed of the content that is present at this moment. But wait and watch :)

---
+++ src/com/olivelabs/loadbalancer/MainClass.java
@@ -0,0 +1,13 @@
+package com.olivelabs.loadbalancer;
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The code is not yet implemented. Nor is design done yet. You can contribute.\n Thanks");
+	}
+
+}
