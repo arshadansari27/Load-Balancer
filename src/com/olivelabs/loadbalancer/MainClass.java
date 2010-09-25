9c3d12bb51e8b4f983d4848031c18c52d050237c
arshadansari27 <arshadansari27@ba3f5f63-9442-bb69-0387-a3d508458a7b>
2010-09-25 19:21:16 +0000

    Test example for Jamil.

--- src/com/olivelabs/loadbalancer/MainClass.java
+++ src/com/olivelabs/loadbalancer/MainClass.java
@@ -9,6 +9,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("The code is not yet implemented. Nor is design done yet. You can contribute.\n Thanks");
 		System.out.println("Testing Preliminary stuff...");
+		System.out.println("Test Jamil...");
 	}
 
 }
