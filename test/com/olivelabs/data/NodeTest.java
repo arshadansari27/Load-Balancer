455cd7b6daf77ce86665a49754c02405fbba9b46
arshadansari27 <arshadansari27@ba3f5f63-9442-bb69-0387-a3d508458a7b>
2010-09-28 20:54:45 +0000

    Some Source code and their respective JUnit Tests are ready.

---
+++ test/com/olivelabs/data/NodeTest.java
@@ -0,0 +1,38 @@
+package com.olivelabs.data;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class NodeTest {
+	Node _node;
+	String _host;
+	String _port;
+	Metric metric;
+	@Before
+	public void setUp() throws Exception{
+		
+		_host = "localhost";
+		_port = "9090";
+		metric = Metric.getMetric(Metric.STRATEGY_REQUEST_SIZE);
+		_node = new Node(_host,_port, metric);
+	}
+	
+	@Test
+	public void testGetHost(){
+		Assert.assertEquals(this._host, _node.getHost());
+	}
+	@Test
+	public void testGetPort(){
+		Long port = Long.parseLong(this._port);
+		Assert.assertEquals(port, _node.getPort());
+	}
+	
+	@Test
+	public void testGetMetric(){
+		Metric metric = _node.getMetric();
+		Assert.assertNotNull(metric);
+		
+	}
+}
