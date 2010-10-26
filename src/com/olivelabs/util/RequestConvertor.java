7935a493ba8d90084065bb037ee0c2d5badcc36f
arshadansari27 <arshadansari27@ba3f5f63-9442-bb69-0387-a3d508458a7b>
2010-10-26 15:10:22 +0000

    Request Response Convertor...

--- src/com/olivelabs/util/RequestConvertor.java
+++ src/com/olivelabs/util/RequestConvertor.java
@@ -1,5 +1,104 @@
 package com.olivelabs.util;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.simpleframework.http.Address;
+import org.simpleframework.http.ContentType;
+import org.simpleframework.http.Cookie;
+import org.simpleframework.http.Form;
+import org.simpleframework.http.Path;
+import org.simpleframework.http.Query;
+import org.simpleframework.http.Request;
+import org.simpleframework.http.session.Session;
+
+import com.ning.http.client.RequestBuilder;
+import com.ning.http.client.RequestType;
+
 public class RequestConvertor {
 
+	public static com.ning.http.client.Request copyRequest(Request serverRequest,
+			String host, String port) throws Throwable {
+		
+		Map<String, String> attributes = serverRequest.getAttributes();
+		String content = serverRequest.getContent();
+		InetSocketAddress clientAddress = serverRequest.getClientAddress();
+		int contentLength = serverRequest.getContentLength();
+		ContentType contentTypeData = serverRequest.getContentType();
+		List<Cookie> cookies = serverRequest.getCookies();
+		String requestMethod = serverRequest.getMethod();
+		List<String> headers = serverRequest.getNames();
+		Session session = serverRequest.getSession();
+		Path path = serverRequest.getPath();
+		Form form = serverRequest.getForm();
+
+		
+
+		RequestBuilder requestBuilder = new RequestBuilder(getRequestType(requestMethod));
+		if((RequestType.PUT).equals(getRequestType(requestMethod)) || (RequestType.POST).equals(getRequestType(requestMethod)) )
+			requestBuilder.setBody(content);
+		for(String header : headers){
+			requestBuilder.addHeader(header, serverRequest.getValue(header));
+		}
+		for(Cookie cookie : cookies){
+			com.ning.http.client.Cookie cookieToSend = new com.ning.http.client.Cookie(cookie.getDomain(),cookie.getName(),cookie.getValue(), cookie.getPath(), cookie.getExpiry(),false); 
+			requestBuilder.addCookie(cookieToSend);
+		}
+		//requestBuilder.setParameters(attributes);
+		Set sessionKeys = session.entrySet();
+		for(Object key : sessionKeys){
+			requestBuilder.addParameter((String) key,(String) session.get(key));
+		}
+		
+		if(port!=null && !("").equals(port)){
+			port=":"+port;
+		}
+		else{
+			port = "";
+		}
+		
+		String protocol =serverRequest.getAddress().getScheme();// +"://";
+		if( protocol==null || ("").equals(protocol)){
+			protocol = "http://";
+		}
+		else{
+			protocol = protocol + "://";
+		}
+		String queryString = "";
+		Query query = serverRequest.getAddress().getQuery();
+		if(!query.isEmpty()){
+			queryString = "?";
+			int size = query.keySet().size();
+			int count=0;
+			for(String key : query.keySet()){
+				queryString += key+"="+query.get(key);
+				if(count!=0 || count<(size-1)) queryString +="&";
+				count++;
+			}
+		}
+		
+		String url = protocol + host + port + serverRequest.getPath()+query;
+		System.out.println(url);
+		requestBuilder.setUrl(url);
+		return requestBuilder.build();
+		
+	}
+	
+	public static RequestType getRequestType(String requestMethod){
+		RequestType requestType = RequestType.GET;
+		if (("POST".equals(requestMethod)))
+			requestType = RequestType.POST;
+		if (("PUT".equals(requestMethod)))
+			requestType = RequestType.PUT;
+		if (("DELETE".equals(requestMethod)))
+			requestType = RequestType.DELETE;
+		if (("HEAD".equals(requestMethod)))
+			requestType = RequestType.HEAD;
+		if (("OPTIONS".equals(requestMethod)))
+			requestType = RequestType.OPTIONS;
+		return requestType;
+	}
 }
--- src/com/olivelabs/util/ResponseConvertor.java
+++ src/com/olivelabs/util/ResponseConvertor.java
@@ -1,16 +1,47 @@
 package com.olivelabs.util;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Set;
+
 import org.simpleframework.http.Response;
+
+import com.ning.http.client.Cookie;
+import com.ning.http.client.Headers;
+import com.ning.http.url.Url;
 
 public class ResponseConvertor {
 
 	public static void copyResponse(com.ning.http.client.Response response,
-			Response serverResponse) {
-
+			Response serverResponse) throws Throwable {
+		String contentType = response.getContentType();
+		List<Cookie> cookiesResponse = response.getCookies();
+		Headers headers = response.getHeaders();
+		String responseBody = response.getResponseBody();
+		int statusCode = response.getStatusCode();
+		String statusText = response.getStatusText();
+		Url responseUrl = response.getUrl();
+		
+		serverResponse.setCode(statusCode);
+		serverResponse.setText(statusText);
+		for(Cookie cookie : cookiesResponse){
+			String name = cookie.getName();
+			String value = cookie.getValue();
+			String domain = cookie.getDomain();
+			int age = cookie.getMaxAge();
+			String path = cookie.getPath(); 
+			serverResponse.setCookie(new org.simpleframework.http.Cookie(name, value, path));
+		}
+		Set<String> headersServerResponse = headers.getHeaderNames();
+		for (String header : headersServerResponse){
+			serverResponse.add(header, headers.getHeaderValue(header));
+		}
+		PrintStream body = serverResponse.getPrintStream();
+		body.print(responseBody);
+		body.close();
+		serverResponse.commit();
 	}
 
-	public static void copyResponse(Response serverResponse,
-			com.ning.http.client.Response response) {
-
-	}
 }
